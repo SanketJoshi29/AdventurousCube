5b1e6af0aae7853000bf3fce04d8cd60e35c0074
SanketJoshi29 <72932643+SanketJoshi29@users.noreply.github.com>
2021-05-22 21:04:06 +0530

    Lock System added

--- Assets/Scripts/EndPointTrigger.cs
+++ Assets/Scripts/EndPointTrigger.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class EndPointTrigger : MonoBehaviour
 {
+
     public Player movement;
     public GameManager gameManager;
     void OnTriggerEnter()
