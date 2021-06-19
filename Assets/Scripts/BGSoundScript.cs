d370a29ac417e64b9b316693667f98e2115a79bf
SanketJoshi29 <72932643+SanketJoshi29@users.noreply.github.com>
2021-06-19 17:45:38 +0530

    Added Music

---
+++ Assets/Scripts/BGSoundScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class BGSoundScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //Play Global
+    private static BGSoundScript instance = null;
+    public static BGSoundScript Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    //Play Gobal End
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/EndPointTrigger.cs
+++ Assets/Scripts/EndPointTrigger.cs
@@ -5,9 +5,11 @@ public class EndPointTrigger : MonoBehaviour
 
     public Player movement;
     public GameManager gameManager;
+    public AudioSource levelCompleteAudio;
     void OnTriggerEnter()
     {
         gameManager.CompleteLevel();
         movement.enabled = false;
+        levelCompleteAudio.enabled = true;
     }
 }
