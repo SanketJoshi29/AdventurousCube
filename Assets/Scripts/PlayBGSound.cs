9c8a6f829d6ba7a34b57516ab3202d3102aa19a3
SanketJoshi29 <72932643+SanketJoshi29@users.noreply.github.com>
2021-06-21 12:06:40 +0530

    Finishing Up

---
+++ Assets/Scripts/PlayBGSound.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayBGSound : MonoBehaviour
+{
+    void Start()
+    {
+        BGSoundScript.Instance.gameObject.GetComponent<AudioSource>().Play();
+    }
+}
+
